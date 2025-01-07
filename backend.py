d65f7c5542af749916f8d5f9375ae9849ea34657
MohanRao58 <mohanraoganala7@gmail.com>
2025-01-07 20:42:04 +0530

    Added Screenshots

--- backend.py
+++ backend.py
@@ -1,20 +1,25 @@
 import requests
-from git.config import needs_values
+from dotenv import load_dotenv
+import os
 
-API_KEY = "240524853066c4040a8324793d5b56e8"
-def get_data(place, forecast_days=None, kind=None):
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the API key
+API_KEY = os.getenv('API_KEY')
+#print(api_key)  # Use your API key in your code
+
+
+
+def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
+    print(get_data(place="Tokyo", forecast_days=3))
--- main.py
+++ main.py
@@ -1,15 +1,45 @@
 import streamlit as st
 import plotly.express as px
 from backend import get_data
-st.title("Weather forecast for the Next Days")
+import math
 
+# Add title, text input, slider, selectbox, and subheader
+st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
-days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the number of forecasted days")
-option = st.selectbox("Select data to view", ("Temperature","Sky"))
+days = st.slider("Forecast Days", min_value=1, max_value=5,
+                 help="Select the number of forecasted days")
+option = st.selectbox("Select data to view",
+                      ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
+    try:
+        # Get the temperature/sky data
+        filtered_data = get_data(place, days)
 
-d, t = get_data(place, days, option)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-figure = px.line(x=d, y=t, labels={"x":"Date", "y":"Temperature (C)"})
-st.plotly_chart(figure)
+
+
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+
+            # Divide the image paths into chunks of 5
+            chunk_size = 5
+            rows = [image_paths[i:i + chunk_size] for i in range(0, len(image_paths), chunk_size)]
+
+            for row in rows:
+                cols = st.columns(len(row))  # Create columns dynamically for the current row
+                for col, image_path in zip(cols, row):
+                    with col:
+                        st.image(image_path, width=115)
+    except KeyError:
+        st.write("Entered Place does not exist!")
